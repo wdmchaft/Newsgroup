f4dcc75793f1b1cc4e6dcd2735dd27818b5c6494
Jim Kubicek <jkubicek@gmail.com>
2011-02-15 18:30:20 -0500

    Moved some files around in the project directory to make the organization more clear.

---
+++ Classes/IndividualThreadView.h
@@ -0,0 +1,16 @@
+//
+//  IndividualThreadView.h
+//  Newsgroup
+//
+//  Created by Jim Kubicek on 2/15/11.
+//  Copyright 2011 jimkubicek.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface IndividualThreadView : UITableViewController {
+
+}
+
+@end
