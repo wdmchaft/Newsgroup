e37916a24f5c1cb4da391f01cf6e4a336c397fb0
Jim Kubicek <jkubicek@gmail.com>
2011-02-15 20:15:49 -0500

    Created a FetchedResultsViewController superclass

---
+++ Classes/FetchedResultsViewController.h
@@ -0,0 +1,16 @@
+//
+//  FetchedResultsViewController.h
+//  Newsgroup
+//
+//  Created by Jim Kubicek on 2/15/11.
+//  Copyright 2011 jimkubicek.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FetchedResultsViewController : UIViewController {
+
+}
+
+@end
