b74ea3882a53bf8d75aea0b2c27c40dfcbab3dbc
Jim Kubicek <jkubicek@gmail.com>
2011-02-24 11:56:55 -0500

    Added TestGPPostLoadOperation test class

---
+++ Test Classes/TestGPPostLoadOperation.m
@@ -0,0 +1,39 @@
+//
+//  TestGPPostLoadOperation.m
+//  Newsgroup
+//
+//  Created by Jim Kubicek on 2/24/11.
+//  Copyright 2011 jimkubicek.com. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+#import "GPPostLoadOperation.h"
+
+@interface TestGPPostLoadOperation : GHTestCase {
+    
+}
+
+@end
+
+@implementation TestGPPostLoadOperation
+
+
+
+- (void)setUpClass {
+}
+
+- (void)tearDownClass {
+}
+
+- (void)setUp {
+}
+
+- (void)tearDown {
+    
+}
+
+- (void)testFetchAllThreads {
+}
+
+
+@end
