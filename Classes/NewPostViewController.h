bcd36e12ded6b540fa088ffcab2b2430c4b897e1
Jim Kubicek <jkubicek@gmail.com>
2011-03-16 12:27:02 -0400

    Added new post view controller and nib

---
+++ Classes/NewPostViewController.h
@@ -0,0 +1,16 @@
+//
+//  NewPostViewController.h
+//  Newsgroup
+//
+//  Created by Jim Kubicek on 3/16/11.
+//  Copyright 2011 jimkubicek.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface NewPostViewController : UIViewController {
+    
+}
+
+@end
