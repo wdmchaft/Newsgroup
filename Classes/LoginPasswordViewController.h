f408b070d7bb11f9aa8660f113f1c3f99e835859
Jim Kubicek <jkubicek@gmail.com>
2011-03-24 19:37:03 -0400

    Created loginpassword view controller and nib

---
+++ Classes/LoginPasswordViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginPasswordViewController.h
+//  Newsgroup
+//
+//  Created by Jim Kubicek on 3/24/11.
+//  Copyright 2011 jimkubicek.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LoginPasswordViewController : UIViewController {
+    
+}
+
+@end
