39bbc7a1d5b532edf7f7425a0d05315f01bce797
Jim Kubicek <jkubicek@gmail.com>
2011-04-01 13:09:38 -0400

    Added an AdHocDeployment script, does nothing at this point.

---
+++ AdHocDeployment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  AdHocDeployment.sh
+#  Newsgroup
+#
+#  Created by Jim Kubicek on 4/1/11.
+#  Copyright 2011 jimkubicek.com. All rights reserved.
+
+#  Update version number
+
+#  Git tag
+
+#  Build and Archive
+
+#  Push to TestFlightApp
