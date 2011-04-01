eaeabe302387d3b5dd2d8239896b42b375c5e4ee
Jim Kubicek <jkubicek@gmail.com>
2011-04-01 16:59:25 -0400

    Update version to v1.0.2.2

--- AdHocDeployment.sh
+++ AdHocDeployment.sh
@@ -7,8 +7,20 @@
 #  Copyright 2011 jimkubicek.com. All rights reserved.
 
 #  Update version number
+agvtool next-version -all
+agvtool new-marketing-version 1.0.2
+
+#  Update commit
+VERS=`agvtool vers -terse`
+MVERS=`agvtool what-marketing-version -terse1`
+TAG="v$MVERS:$VERS"
+COMMIT_MSG="Update version to $TAG"
+
+git add .
+git commit -m $COMMIT_MSG
 
 #  Git tag
+git tag $TAG -m "$COMMIT_MSG"
 
 #  Build and Archive
 
