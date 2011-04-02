087551b0f8ecce1a3302273d8c82c4bd277d5b7f
Jim Kubicek <jkubicek@gmail.com>
2011-04-01 21:32:37 -0400

    Added the post history view controller

---
+++ Classes/PostHistoryViewController.h
@@ -0,0 +1,17 @@
+//
+//  PostHistoryViewController.h
+//  Newsgroup
+//
+//  Created by Jim Kubicek on 4/1/11.
+//  Copyright 2011 jimkubicek.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FetchedResultsViewController.h"
+
+
+@interface PostHistoryViewController : FetchedResultsViewController {
+    
+}
+
+@end
